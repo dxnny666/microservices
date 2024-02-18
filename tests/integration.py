cbbc105a30a8af5bcb96dead22bd64845bb9b061
boyarkov <94081720+dxnny666@users.noreply.github.com>
2024-02-18 23:44:42 +0300

    pls

--- tests/integration.py
+++ tests/integration.py
@@ -15,7 +15,7 @@ def check_connect():
             dbname='TicketAirport',
             user='postgres',
             password='password',
-            host='database',
+            host='localhost',
             port='5432'
         )
         conn.close()
@@ -32,13 +32,12 @@ class TestIntegration(unittest.TestCase):
         self.assertEqual(check_connect(), True)
 
     def test_ticket_service_connection(self):
-        r = requests.get("http://ticket_service:8000/health", verify=False)
+        r = requests.get("http://localhost:8000/health", verify=False)
         self.assertEqual(r.status_code, 200)
 
     def test_statistics_service_connection(self):
-        r = requests.get("http://statistics_server:8001/health", verify=False)
+        r = requests.get("http://localhost:8001/health", verify=False)
         self.assertEqual(r.status_code, 200)
-
 
     def test_ticket_get(self):
         res = requests.get(f"{get_ticket_by_id_url}?ticket_id=1").json()
