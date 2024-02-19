4dc6ee54e5f5998e5462c3791473d2a4bdae8090
shalaev <artem_klim1@mail.ru>
2024-02-19 18:56:52 +0300

    fix delete

--- ticket_service/app/main.py
+++ ticket_service/app/main.py
@@ -67,6 +67,7 @@ async def delete_ticket(ticket_id: int, db: db_dependency):
     try:
         ticket = db.query(Ticket).filter(Ticket.id == ticket_id).first()
         db.delete(ticket)
+        db.commit()
         return "Success"
     except Exception as e:
         return "cant find ticket"
